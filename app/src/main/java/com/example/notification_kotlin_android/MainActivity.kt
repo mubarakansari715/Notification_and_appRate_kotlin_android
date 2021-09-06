f7edbf601b10e40a250a32bed48e9c243248fd76
Mubarak Ansari <54958497+mubarakansari715@users.noreply.github.com>
2021-09-06 12:20:57 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/notification_kotlin_android/MainActivity.kt
+++ app/src/main/java/com/example/notification_kotlin_android/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.notification_kotlin_android
+package com.example.notification_kotlin
 
 import android.app.*
 import android.content.Context
@@ -8,6 +8,7 @@ import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import hotchemi.android.rate.AppRate
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -22,28 +23,39 @@ class MainActivity : AppCompatActivity() {
         notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         btn_notification.setOnClickListener {
-            var intent = Intent(this, LauncherActivity::class.java)
+            val intent = Intent(this, LauncherActivity::class.java)
             val pendingIntent =
                 PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-
-                notificationChanel = NotificationChannel(channelId, des, NotificationManager.IMPORTANCE_HIGH)
+                notificationChanel =
+                    NotificationChannel(channelId, des, NotificationManager.IMPORTANCE_HIGH)
                 notificationChanel.enableLights(true)
                 notificationChanel.lightColor = Color.GREEN
                 notificationChanel.enableVibration(true)
                 notificationManager.createNotificationChannel(notificationChanel)
-
                 bulder = Notification.Builder(this, channelId)
                     .setContentTitle("Brainvire Info Tech")
                     .setContentText("Welcome to Brainvire")
                     .setSmallIcon(R.drawable.ic_baseline_notifications_24)
-                    .setLargeIcon(BitmapFactory.decodeResource(this.resources,
-                        R.drawable.ic_baseline_notifications_24
+                    .setLargeIcon(
+                        BitmapFactory.decodeResource(
+                            this.resources,
+                            R.drawable.ic_baseline_notifications_24
                         )
                     )
                     .setContentIntent(pendingIntent)
             }
             notificationManager.notify(1234, bulder.build())
         }
+
+        AppRate.with(this)
+            .setInstallDays(0)
+            .setLaunchTimes(2)
+            .setRemindInterval(2)
+            .monitor()
+        AppRate.showRateDialogIfMeetsConditions(this)
+        AppRate.with(this).clearAgreeShowDialog()
+        AppRate.with(this).shouldShowRateDialog()
+
     }
 }
